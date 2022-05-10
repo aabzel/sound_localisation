62cc59c6ff4c4ef0992662bc1681e9310293841d
Alex Bar <aabzel@yandex.ru>
2022-05-10 14:14:16 +0300

    Update i2s.c

--- i2s.c
+++ i2s.c
@@ -1,38 +1,3 @@
-/**
-  ******************************************************************************
-  * File Name          : I2S.c
-  * Description        : This file provides code for the configuration
-  *                      of the I2S instances.
-  ******************************************************************************
-  *
-  * COPYRIGHT(c) 2016 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "i2s.h"
 
 #include "gpio.h"
@@ -44,7 +9,6 @@
 #include "fifo.h"
 #endif 
 
-/* USER CODE BEGIN 0 */
 HAL_StatusTypeDef status1, status2;
 uint8_t i2sTxDone=0;
 uint8_t i2sRxDone=0;
@@ -55,8 +19,6 @@ int16_t i2sDataIn[AMMOUNT_OF_STEREO_SAMPLES*2];
 int16_t  irrOutVal;
 
 int16_t    g_volThreshold=100;
-    
-/* USER CODE END 0 */
 
 I2S_HandleTypeDef hi2s2;
 
