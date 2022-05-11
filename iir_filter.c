f476db70c3fd2ab044072f7054bf04bac469cde5
Alex Bar <aabzel@yandex.ru>
2022-05-11 17:48:06 +0300

    Update iir_filter.c

--- iir_filter.c
+++ iir_filter.c
@@ -1,23 +1,10 @@
-/**
-  ******************************************************************************
-  * File Name          : iir_filter.c
-  */
 
 #ifdef DIGITAL_MIC_ECHO_EFFECT    
   
     
     
-/* Includes ------------------------------------------------------------------*/
 #include "iir_filter.h"
-//#include < string.h > 
-/* USER CODE BEGIN 0 */
 
-/* USER CODE END 0 */
-
-/*----------------------------------------------------------------------------*/
-/* Configure iir_filter                                                             */
-/*----------------------------------------------------------------------------*/
-/* USER CODE BEGIN 1 */
 int32_t g_fifoArray[SIZE_OF_FIFO];
 int32_t g_curElem    =0;
 int32_t g_sizeofFifo =0;
@@ -27,8 +14,7 @@ int32_t valtoFifo    =0;
 int16_t g_threshold=10000;// -32k ... +32k
 /* USER CODE END 1 */
 
-void initFirFifo()
-{
+void initFirFifo(){
   valfromFifo=0; 
   g_curElem=0;
   valtoFifo=0;
@@ -37,34 +23,27 @@ void initFirFifo()
   
 }
 
-void putFIFOval(int32_t inVal)
-{
-    if(g_curElem < g_sizeofFifo)
-    {
+void putFIFOval(int32_t inVal){
+    if(g_curElem < g_sizeofFifo)    {
         g_fifoArray[g_curElem] = inVal;
     }
-    else
-    {// ùåë÷îê
+    else    {// Ã¹Ã¥Ã«Ã·Ã®Ãª
         g_curElem = 0;
         g_fifoArray[g_curElem] = inVal;
     }
     g_curElem++;
 }
 
-int32_t getFIFOVal()
-{
-    if(g_curElem < (g_sizeofFifo-1))
-    {
+int32_t getFIFOVal(){
+    if(g_curElem < (g_sizeofFifo-1))    {
         return g_fifoArray[g_curElem+1];
     }
-    else
-    {
+    else    {
         return g_fifoArray[0];
     }
 }
 
-void iirFilter(int16_t inVal, int16_t  *outVal)
-{
+void iirFilter(int16_t inVal, int16_t  *outVal){
   valfromFifo = getFIFOVal();
   *outVal = (inVal + valfromFifo);
   valtoFifo = (*outVal)*2/5; 
@@ -72,18 +51,14 @@ void iirFilter(int16_t inVal, int16_t  *outVal)
   
 }
 
-void distortion(int16_t inVal, int16_t  *outVal)
-{
-  if(g_threshold<inVal)
-  {
+void distortion(int16_t inVal, int16_t  *outVal){
+  if(g_threshold<inVal)  {
     *outVal=g_threshold;
     return;
-  }else if(inVal<-g_threshold)
-  {
+  }else if(inVal<-g_threshold)  {
     *outVal=-g_threshold;
     return;
-  }else
-  {
+  }else  {
     *outVal=inVal;
   }
 }
